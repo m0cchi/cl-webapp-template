2272495a6eae2ada6ef0d664e40d6fba0f2576e3
mocchi <boom.boom.planet@gmail.com>
2015-11-07 12:02:28 +0900

    add lwrap-util

---
+++ woo-lwrap-util.lisp
@@ -0,0 +1,34 @@
+(provide 'woo.lwrap.util)
+
+(defpackage :woo.lwrap.util
+  (:use :cl)
+  (:export make-keyword split-string make-map))
+
+(in-package :woo.lwrap.util)
+
+(defun make-keyword (name)
+  (values
+   (intern 
+    (string-upcase name)
+    :keyword)))
+
+(defun split-string (word string)
+  (let ((i (search word string))
+        (retval '()))
+    (if i
+        (progn
+          (setq retval (list (subseq string 0 i)))
+          (let* ((retval2 (split-string word
+                                        (subseq string (+ i 1)))))
+            (setq  retval `(,@retval ,@retval2))))
+      (setq retval (list string)))
+    retval))
+
+(defun make-map (params)
+  (if (= 0
+         (mod (list-length params) 2))
+      (dotimes (i (/ (list-length params) 2))
+        (setf (nth (* i 2) params)
+              (make-keyword (nth (* i 2) params)))))
+  params)
+
